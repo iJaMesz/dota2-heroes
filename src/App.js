efc49b274bc642881a92b93d09f76a7b9a613fee
James Reid <jamestreid@hotmail.co.uk>
2022-01-27 23:13:35 +0000

    Added all strength heroes. search props. css width

--- src/App.js
+++ src/App.js
@@ -6,62 +6,17 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      heroes: [
-        {
-          id: 1,
-          name: "Abbadon",
-          full_name: 'Abbadon, The Lord of Avernus',
-          image_url: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/abaddon.png'
-        },
-        {
-          id: 2,
-          name: "Alchemist",
-          full_name: 'Razzil Darkbrew, The Alchemist',
-          image_url: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/alchemist.png'
-        },
-        {
-          id: 3,
-          name: "Axe",
-          full_name: 'Mogul Khan, the Axe',
-          image_url: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/axe.png'
-        },
-        {
-          id: 4,
-          name: "Beastmaster",
-          full_name: 'Karroch, the Beastmaster',
-          image_url: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/beastmaster.png'
-        },
-        {
-          id: 5,
-          name: "Brewmaster",
-          full_name: 'Mangix, the Brewmaster',
-          image_url: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/brewmaster.png'
-        },
-        {
-          id: 6,
-          name: "Bristleback",
-          full_name: 'Rigwarl, the Bristleback',
-          image_url: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/bristleback.png'
-        },
-        {
-          id: 7,
-          name: "Centaur Warrunner",
-          full_name: 'Bradwarden, the Centaur Warrunner',
-          image_url: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/centaur.png'
-        },
-        {
-          id: 8,
-          name: "Chaos Knight",
-          full_name: 'Chaos Knight',
-          image_url: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/chaos_knight.png'
-        },
-      ],
-    };
+      heroes: require('./resources/heroes.json').heroes,
+      searchField: ''
+    }
   }
   render() {
+    const {heroes, searchField} = this.state;
+    const filteredHeroes = searchField ? heroes.filter(h => h.name.toLowerCase().includes(searchField.toLowerCase())) : heroes;
     return (
       <div className="App">
-        <CardList heroes={this.state.heroes}></CardList>
+        <input type="search" placeholder="Search for a Hero" onChange={e => this.setState({searchField: e.target.value}, () => console.log(e.target.value))}></input>
+        <CardList heroes={filteredHeroes}></CardList>
       </div>
     );
   }
