28b55c60d88f7a71c297c6feef0fccb026e08175
James Reid <jamestreid@hotmail.co.uk>
2022-01-30 00:28:04 +0000

    search box - rename to d2-heroes-app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 import { Component } from "react";
 
 class App extends Component {
@@ -10,12 +11,17 @@ class App extends Component {
       searchField: ''
     }
   }
+
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value});
+  }
+
   render() {
     const {heroes, searchField} = this.state;
     const filteredHeroes = searchField ? heroes.filter(h => h.name.toLowerCase().includes(searchField.toLowerCase())) : heroes;
     return (
       <div className="App">
-        <input type="search" placeholder="Search for a Hero" onChange={e => this.setState({searchField: e.target.value})}></input>
+        <SearchBox placeholder='Search for Heroes' handleChange={this.handleChange}></SearchBox>
         <CardList heroes={filteredHeroes}></CardList>
       </div>
     );
